a3316f0155e46b48102428436d511c38519d9750
pahlavaubivca <pahlavaubivca@gmail.com>
2024-04-28 21:23:21 +0300

    [*] try to fix data send

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use rppal::gpio::Gpio;
-use rppal::uart::{Parity, Uart};
+use rppal::uart::{Parity, Queue, Uart};
 
 fn main() {
     println!("Hello, world!");
@@ -12,7 +12,22 @@ fn main() {
 
     // let mut serial = serial::Serial::new(txd_pin, rxd_pin, 9600);
 
-    let mut uart_result = Uart::new(19_200, Parity::None, 8, 1);
+    // let mut uart_result = Uart::with_path(
+    //     "/dev/ttyAMA0",
+    //     // "/dev/ttyS0",
+    //     19200,
+    //     Parity::None,
+    //     8,
+    //     1,
+    // );
+
+    let mut uart_result = Uart::new(
+        19200,
+        Parity::None,
+        8,
+        1,
+    );
+
     let mut uart;
     match uart_result {
         Ok(_uart) => {
@@ -26,17 +41,30 @@ fn main() {
     }
 
     println!("UART initialized");
+    uart.flush(Queue::Both).unwrap();
 
     println!("UART set write mode");
-    uart.set_write_mode(false).unwrap();
+    uart.set_write_mode(true).unwrap();
     let str_to_send = "h";
 
     loop {
-        println!("UART trying to send data");
+        println!("UART trying to send data 1");
+        // uart.flush(Queue::Input).unwrap();
+        // uart.flush(Queue::Output).unwrap();
+        uart.flush(Queue::Both).unwrap();
+        uart.send_start().unwrap();
         let res = uart.write(str_to_send.as_bytes());
-        if res.is_err() {
-            println!("Error send data: {:?}", res.err());
+        match res {
+            Ok(bs) => {
+                println!("UART sent data, size: {}", bs);
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
         }
+
+        uart.send_stop().unwrap();
+
 
         println!("Sent: {:?}", str_to_send);
         thread::sleep(Duration::from_secs(1));
