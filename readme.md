1574d6f4d589eb28c863ee95bbf97a101ddd35f4
pahlavaubivca <pahlavaubivca@gmail.com>
2024-08-18 13:12:40 +0300

    [*] cleanup

--- readme.md
+++ readme.md
@@ -1,15 +1,16 @@
-## Run
-``
+## Build binary for Raspberry Pi
+
+` cargo build --target aarch64-unknown-linux-gnu --release`
 
 ### Setup UART
 
-    1) remove `console=serial0,115200` from `/boot/firmware/cmdline.txt`
+1) remove `console=serial0,115200` from `/boot/firmware/cmdline.txt`
 2) add `dtparam=uart=on` to `/boot/firmware/config.txt`
 3) remove `enable_uart` from `/boot/firmware/config.txt`
 3) add `dtoverlay=disable-bt` to `/boot/firmware/config.txt` (this step need verification)
 4) stop bluetooth service `sudo systemctl disable hciuart` (need verification)
 
-## FAQ 
+## FAQ
 
 ### raspberry pi Error: Io(Os { code: 13, kind: PermissionDenied, message: "Permission denied" })
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::thread;
 use std::time::Duration;
 use rppal::gpio::Gpio;
 use rppal::uart::{Parity, Queue, Uart};
@@ -64,77 +63,6 @@ fn main() {
                 println!("Error reading from UART: {:?}", err);
             }
         }
-        //
-        //
-        // if let Ok(read_res) = uart.read(&mut short_buf) {
-        //     if read_res > 0 {
-        //         println!("UART read size: {}", read_res);
-        //         println!("UART read buf: {:?}", short_buf);
-        //         let slice = &short_buf[0..read_res];
-        //         match std::str::from_utf8(&short_buf) {
-        //             Ok(s) => {
-        //                 message.push_str(s);
-        //                 if message.contains("\r\n") {
-        //                     println!("Message: {:?}", message);
-        //                     message = String::new();
-        //                     // match uart.flush(Queue::Both) {
-        //                     //     Ok(res) => {
-        //                     //         println!("Flush queue output result: {:?}", res);
-        //                     //     }
-        //                     //     Err(err) => {
-        //                     //         println!("Flush queue output error: {:?}", err);
-        //                     //     }
-        //                     // }
-        //                 }
-        //
-        //                 println!("UART read: {:?}", s);
-        //             }
-        //             Err(e) => {
-        //                 println!("Error: {:?}", e);
-        //             }
-        //         }
-        //
-        //     } else {
-        //         // println!("No data read from UART");
-        //     }
-        // } else {
-        //     println!("Error reading from UART");
-        // }
-
-
-        // println!("UART trying to write data");
-        // let raw_str = "top_row=127.0.0.1/100&bottom_row=<-modes/select/settings->;";
-        // let let_of_raw_str = raw_str.len() as u32;
-        // 
-        // let mut str_len_in_bytes = let_of_raw_str.to_be_bytes().to_vec();
-        // let raw_str_bytes = raw_str.as_bytes().to_vec();
-        // str_len_in_bytes.extend(raw_str_bytes);
-        // println!("str_len_in_bytes: {:?}", str_len_in_bytes);
-        // // uart.flush(Queue::Input).unwrap();
-        // 
-        // let res = uart.write(str_len_in_bytes.as_slice());
-        // match res {
-        //     Ok(bs) => {
-        //         println!("UART sent data, size: {}", bs);
-        //     }
-        //     Err(e) => {
-        //         println!("Error: {:?}", e);
-        //     }
-        // }
-
-
-        // uart.send_stop().unwrap();
-        // println!("loop...");
-
-        // println!("Sent: {:?}", str_to_send);
-
-
-        // thread::sleep(Duration::from_millis(500));
-
-        // let mut buf = [0; 1];
-        // uart.read(&mut buf)?;
-        // println!("Received: {:?}", buf[0] as char);
+        
     }
-
-    // uart.
 }
